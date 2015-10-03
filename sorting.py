e14d18a324eeeca60c7efd30243091e88ce46d7e
mbartoli <bartolimichael@gmail.com>
2015-10-02 20:52:10 -0700

    adding quicksort

--- sorting.py
+++ sorting.py
@@ -10,6 +10,48 @@ Sorting algorithms currently included:
 
 """
 
+def quicksort(unsorted,lo=0,hi=None):
+	""" 
+	Quicksort -- Worst: O(n^2), Best: O(n log n), Average: O(n log n)
+
+	Quicksort is a divide and conquer algorithm. Quicksort first divides a large
+	array into two smaller sub-arrays: the low elements and the high elements.
+	Quicksort can then recursively sort the sub-arrays.
+
+	The steps are:
+
+	Pick an element, called a pivot, from the array.
+	Reorder the array so that all elements with values less than the pivot come
+		before the pivot, while all elements with values greater than the
+		pivot come after it (equal values can go either way). After this
+		partitioning, the pivot is in its final position. This is called the
+		partition operation.
+	Recursively apply the above steps to the sub-array of elements with smaller 
+		values and separately to the sub-array of elements with greater
+		values.
+	
+	The base case of the recursion is arrays of size zero or one, which never 
+	need to be sorted. A quicksort that sorts elements lo through hi (inclusive) 
+	of an array A can be expressed as function quicksort(A, lo, hi). Sorting the 
+	entire array is accomplished by calling quicksort(A, 1, length(A)).
+	"""
+	less = []
+	equal = []
+	greater = []
+
+	if len(unsorted) > 1:
+		pivot = unsorted[0]
+		for x in unsorted:
+		    	if x < pivot:
+				less.append(x)
+		    	if x == pivot:
+				equal.append(x)
+		    	if x > pivot:
+				greater.append(x)
+		return quicksort(less)+equal+quicksort(greater) 
+	else: 
+		return unsorted
+
 
 def insertion_sort(unsorted):
 	""" 
@@ -129,7 +171,7 @@ def main():
 	assert insertion_sort(unsorted) == sorted
 	assert merge_sort(unsorted) == sorted
 	assert selection_sort(unsorted) == sorted
-
+	assert quicksort(unsorted) == sorted
 
 if __name__ == '__main__':
 	main()
